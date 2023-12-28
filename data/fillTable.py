b0222690626e09e7c8b8d8a6bba62a208469f2a4
Qiyue Chen <jackchen20048@gmail.com>
2023-12-28 13:25:42 -0500

    Initial Commit

---
+++ data/fillTable.py
@@ -0,0 +1,50 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect('database.db')
+
+f = open ('./responsecopy.json', "r")
+data = json.loads(f.read())
+
+keys = data.keys()
+values = data.values()
+
+keysli = list(keys)
+valuesli = list(values)
+
+def idFill():
+    cur = conn.cursor()
+    with conn:
+        for key in keys:
+            insertid = ("INSERT INTO test (tank_id) VALUES (%s)" % int(key))
+            cur.execute(insertid)
+
+    cur.close()
+
+
+def nameFill():
+    cur = conn.cursor()
+    with conn:
+        for value in values:
+            insertName = ("INSERT INTO test (name) VALUES (?)")
+            name = (value['name'], )
+            cur.execute(insertName, name)
+
+    cur.close()
+
+def Fill():
+    cur = conn.cursor()
+    with conn:
+        insertstr = "INSERT INTO test (tank_id, name, nation, is_premium, tier, type, image_preview, image_normal) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        for i in range(535):
+            insertTuple = (keysli[i], valuesli[i]['name'], valuesli[i]['nation'], 
+                           valuesli[i]['is_premium'], valuesli[i]['tier'], valuesli[i]['type'], 
+                           valuesli[i]['images']['preview'], valuesli[i]['images']['normal'])
+            cur.execute(insertstr, insertTuple)
+    cur.close()
+
+Fill()
+
+conn.commit()
+conn.close()
+f.close()
