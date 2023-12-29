05ba9011ab89901d55771da9860cc85d4398c7b1
Qiyue Chen <jackchen20048@gmail.com>
2023-12-29 13:44:37 -0500

    clean slate

--- wotbStats/members/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Test
-
-def members(request):
-    data = Test.objects.all()
-    context = {"tanks": data}
-    return render(request, 'tankTable.html', context)
