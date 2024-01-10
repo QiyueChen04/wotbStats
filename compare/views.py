84d26a825b2c295480ea373ea171c3cd643bd6fa
Qiyue Chen <jackchen20048@gmail.com>
2024-01-10 01:17:15 -0500

    modified new.html and views.py

--- compare/views.py
+++ compare/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from .forms import SearchForm
-from extApi.models import Tank
+from extApi.models import *
 
 def compare(request):
     if request.method == 'GET':
         form = SearchForm(request.GET)
         if form.is_valid():
             search_query = form.cleaned_data['search_query']
-            search_results = Tank.objects.filter(name__icontains = search_query)
+            tank_results = Tank.objects.filter(name__icontains = search_query)
             context = {
                 'form': form, 
                 'query': search_query, 
-                'results': search_results
+                'tank_results': tank_results,
             }
             return render(request, 'new.html', {'context': context})
 
