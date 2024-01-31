0cdb4caa171aed20fd1f95fc46c83a9b7207d8d9
Qiyue Chen <jackchen20048@gmail.com>
2024-01-31 17:01:45 -0500

    changed some things

--- compare/urls.py
+++ compare/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.compare, name = ""),
+    path("/<str:tank_name1>/<str:tank_name2>", views.compare, name = ""),
 ]
--- compare/views.py
+++ compare/views.py
@@ -2,19 +2,31 @@ from django.shortcuts import render
 from .forms import SearchForm
 from extApi.models import *
 
-def compare(request):
-    if request.method == 'GET':
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            search_query = form.cleaned_data['search_query']
-            tank_results = Tank.objects.filter(name__icontains = search_query)
-            context = {
-                'form': form, 
-                'query': search_query, 
-                'tank_results': tank_results,
-            }
-            return render(request, 'new.html', {'context': context})
+def compare(request, tank_name1, tank_name2):
+    if(tank_name2 == ""):
+        tank1 = Tank.objects.filter(name= tank_name1)
+        context = {
+            'form': SearchForm(request.GET),
+            'tanks': tank1
+        }
+        return render(request, 'compare.html', {'context': context})
+    else:
+        pass
 
-    form = SearchForm()
-    context = {'form': form}
-    return render(request, 'new.html', {'context': context})
+
+# def compare(request, tank_name1, tank_name2):
+#     if request.method == 'GET':
+#         form = SearchForm(request.GET)
+#         if form.is_valid():
+#             search_query = form.cleaned_data['search_query']
+#             tank_results = Tank.objects.filter(name__icontains = search_query)
+#             context = {
+#                 'form': form, 
+#                 'query': search_query, 
+#                 'tank_results': tank_results,
+#             }
+#             return render(request, 'new.html', {'context': context})
+
+#     form = SearchForm()
+#     context = {'form': form}
+#     return render(request, 'new.html', {'context': context})
