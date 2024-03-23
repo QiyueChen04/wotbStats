ff653832505f1425cba18df3927881df84f715ec
Qiyue Chen <jackchen20048@gmail.com>
2024-03-23 01:22:49 -0400

    updated gitignore

--- extApi/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tank
-
-class extApiAdmin(admin.ModelAdmin):
-    list_display = ("tank_id", "name", "image_preview", "image_normal", "nation", "is_premium", "tier", "type")
-
-admin.site.register(Tank, extApiAdmin)
--- extApi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.already_saved_extApi_response),
-]
